9b24b84cc251dd0b986036a5cde1c3cfe81b7648
mehru-sama <meherunnesaoishi514@gmail.com>
2023-11-12 20:54:35 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+music = input("What type of music you are going to perform? ")
+city = input("Which city do you live? ")
+favourite_color = input("What is your favourite color? ")
+band_name = city + ' ' + favourite_color + ' ' + music
+print("Your band name is: " + band_name)
